7962992602eb6109eeaec730791d6bc85326ecfb
SopanhaSem <semsopanha072@gmail.com>
2024-06-01 20:11:44 +0700

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -28,6 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
         dropdown.classList.add("hidden");
       }, 100);
     });
+
+    // Handle click for mobile view
+    button.addEventListener("click", () => {
+      dropdown.classList.toggle("hidden");
+    });
   };
 
   setupDropdown("cryptoDropdownButton", "cryptoDropdown");
@@ -47,31 +52,4 @@ document.getElementById("loginButton").addEventListener("click", function () {
 });
 document.getElementById("signUpButton").addEventListener("click", function () {
   window.location.href = "/src/screen/signup.html";
-});
-//---
-document.addEventListener("DOMContentLoaded", () => {
-  const cryptoDropdownButton = document.getElementById(
-    "exchangeDropdownButton"
-  );
-  const cryptoDropdown = document.getElementById("exchangeDropdown");
-
-  cryptoDropdownButton.addEventListener("mouseenter", () => {
-    cryptoDropdown.classList.remove("hidden");
-  });
-
-  cryptoDropdownButton.addEventListener("mouseleave", () => {
-    setTimeout(() => {
-      if (!cryptoDropdown.matches(":hover")) {
-        cryptoDropdown.classList.add("hidden");
-      }
-    }, 100);
-  });
-
-  cryptoDropdown.addEventListener("mouseleave", () => {
-    cryptoDropdown.classList.add("hidden");
-  });
-
-  cryptoDropdown.addEventListener("mouseenter", () => {
-    cryptoDropdown.classList.remove("hidden");
-  });
 });
