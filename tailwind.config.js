1754f9bb5cbb83f362299785d435d5d119ce0d9a
SopanhaSem <semsopanha072@gmail.com>
2024-06-04 15:32:26 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px", // Adjust the breakpoint value if needed
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
